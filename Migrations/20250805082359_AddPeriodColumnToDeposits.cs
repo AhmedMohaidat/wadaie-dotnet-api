fd76f2572feb3d8ffd884c054308c5ee432b2852
AhmedMohaidat <mohaidatahmed@gmail.com>
2025-08-06 11:04:54 +0300

    GitHub Actions

---
+++ Migrations/20250805082359_AddPeriodColumnToDeposits.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TimeDepositAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPeriodColumnToDeposits : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "CrowdDepositOffers");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Balance",
+                table: "Users",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Period",
+                table: "Deposits",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Deposits",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Period",
+                table: "CrowdDepositOffers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "CrowdDepositOffers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "CustomDepositRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    DurationInDays = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomDepositRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CustomDepositRequests_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomDepositRequests_UserId",
+                table: "CustomDepositRequests",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomDepositRequests");
+
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Period",
+                table: "Deposits");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Deposits");
+
+            migrationBuilder.DropColumn(
+                name: "Period",
+                table: "CrowdDepositOffers");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "CrowdDepositOffers");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "CrowdDepositOffers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
